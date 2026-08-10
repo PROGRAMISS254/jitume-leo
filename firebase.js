a8231d5c897014deff56bb33105e6a68c3343f37
PROGRAMISS254 <programiss254@gmail.com>
2026-08-10 23:02:01 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
-```javascript
 // ==========================================
 // JITUME LEO - FIREBASE CONFIGURATION
 // ==========================================
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDH6IpUKjF9vyP8SplqZUkkUGUp6DNVmQA",
@@ -14,7 +15,8 @@ const firebaseConfig = {
     appId: "1:214590713878:web:26fae4fd25956b24738537"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Make app available to other files
 export { app };
-```
